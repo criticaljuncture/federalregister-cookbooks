9f57a26c5ce07bb407034851c908de6de1c9da43
Peregrinator <bob.burbach@gmail.com>
2014-10-30 14:29:20 -0700

    Fix syntax error when using rewind (must specify the resource type we’re rewinding - ‘template’ here).

--- passenger_apache2/recipes/default.rb
+++ passenger_apache2/recipes/default.rb
@@ -8,11 +8,10 @@ require 'chef/rewind'
 
 include_recipe "passenger_apache2"
 
-rewind "#{node['apache']['dir']}/mods-available/passenger.conf" do
+rewind "template[#{node['apache']['dir']}/mods-available/passenger.conf]" do
     cookbook 'passenger_apache2_federalregister'
     source 'passenger.conf.erb'
     owner 'root'
     group 'root'
     mode 0644
 end
-
