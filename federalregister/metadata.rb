9b47b97247374173ffefe50c2a221fdd9693b171
Peregrinator <bob.burbach@gmail.com>
2015-09-15 15:25:47 -0700

    Add cron jobs for gpo_image imports

--- federalregister/metadata.rb
+++ federalregister/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'info@criticaljuncture.org'
 license          'Apache 2.0'
 description      'Installs/Configures federalregister'
 long_description 'Installs/Configures federalregister'
-version          '0.1.1'
+version          '0.1.2'
 
 #log rotation
 depends "s3cmd_federalregister", '~> 0.1.0'
